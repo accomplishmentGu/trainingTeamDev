6fbec4b6930ff1126c1edd4d096aea789310a0ed
GuZheran <103421073+GuZheran@users.noreply.github.com>
2022-04-17 18:23:22 +0800

    Create IOtemplate.cpp

---
+++ IOtemplate.cpp
@@ -0,0 +1,75 @@
+
+
+#include <bits/stdc++.h>
+
+using namespace std;
+
+namespace IO {
+	
+	template <typename type>
+	inline void Read (type &theNum) {
+	
+		char theChar = 0;
+		int negative = 0; 
+		
+		theChar = getchar();
+		negative = 1;
+		
+		while (theChar < '0' || theChar > '9') {
+			// if the input must be nonnegative, we do not need to write
+			if (theChar == '-') {
+				negative = -1;
+			}
+			
+			theChar = getchar();
+		}
+		
+		theNum = theChar ^ 48; // theChar - 48
+		
+		while ((theChar = getchar()) > '0' - 1 && theChar < '9' + 1) {
+			theNum = (theNum << 3) + (theNum << 1) + (theChar ^ 48);
+		}
+		
+		return theNum * negative;
+		
+	}
+	
+	template <typename type>
+	inline void RecursionWrite (type theNum) {
+		
+		if (theNum) {
+			RecursionWrite(theNum / 10);
+			putchar(theNum % 10 + 48);
+		}
+		
+	}
+	
+	template <typename type1, typename type2>
+	inline void Write (type1 theNum, type2 endChar) {
+		
+		if (!theNum) {
+			putchar('0');
+		}
+		
+		if (theNum < 0) {
+			putchar('-');
+			theNum *= -1;
+		}
+		
+		if (theNum) {
+			RecursionWrite(theNum / 10);
+			putchar(theNum % 10 + 48);
+		}
+		
+		putchar(endChar);
+		
+	}
+	
+}
+
+using namespace IO;
+
+int main () {
+  
+	return 0;
+}
