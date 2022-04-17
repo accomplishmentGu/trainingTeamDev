7211873ac85eda6f5149de1c6a5b64a44b4f3d2b
GuZheran <103421073+GuZheran@users.noreply.github.com>
2022-04-17 18:05:24 +0800

    Update T235837.cpp

--- T235837.cpp
+++ T235837.cpp
@@ -1,1 +1,54 @@
 
+
+#include <iostream>
+#include <fstream>
+#include <vector>
+
+using namespace std;
+
+const int mod = 1e9 + 7;
+
+int _maxi = 0;
+int _num = 0;
+vector<int> _resList;
+long long _myMap[5009][5009];
+
+void CparseIn () {
+	
+	cin >> _maxi >> _num;
+	
+}
+
+void Core () {
+	
+	_myMap[0][0] = 1;
+	
+	for (int i = 1; i < _num + 1; i++) {
+		for (int m = 1; m < _maxi + 1; m++) {
+			if (i > m) {
+				continue;
+			}
+			
+			_myMap[i][m] = (_myMap[i - 1][m - 1] + _myMap[i][m - 1] * i) % mod;
+		}
+	}
+	
+}
+
+void CwriteOut () {
+	
+	cout << _myMap[_num][_maxi] << '\n';
+	
+}
+
+int main () {
+	
+	ios::sync_with_stdio(false);
+	cin.tie(nullptr), cout.tie(nullptr);
+	
+	CparseIn();
+	Core();
+	CwriteOut();
+	return 0;
+	
+}
